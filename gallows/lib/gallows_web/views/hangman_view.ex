38e18b4a24b2c3b0346d613fb237f4bc4809bcc0
Scott Hedrick <me@scottshedrick.com>
2018-02-05 13:15:20 -0600

    added ui feedback for phoenix app

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,26 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses %{
+    :won          =>  { :success, "You Won!" },
+    :lost         =>  { :danger,  "You Lost!" },
+    :good_guess   =>  { :success, "Good Guess!" },
+    :bad_guess    =>  { :warning, "Bad Guess!" },
+    :already_used =>  { :info,    "You already guessed that." },
+  }
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
+
 end
