c68c892fedda826e4e7bd2a1e2f14693e2f47f1c
Scott Hedrick <me@scottshedrick.com>
2018-02-05 13:33:41 -0600

    broke out some view logic into a helper module

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,35 +1,14 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import Gallows.Views.Helpers.GameStateHelper
+
   def game_over?(%{ game_state: game_state }) do
     game_state in [ :won, :lost ]
   end
 
   def new_game_button(conn) do
     button("New Game", to: hangman_path(conn, :create_game) )
-  end
-
-  @responses %{
-    :won          =>  { :success, "You Won!" },
-    :lost         =>  { :danger,  "You Lost!" },
-    :good_guess   =>  { :success, "Good Guess!" },
-    :bad_guess    =>  { :warning, "Bad Guess!" },
-    :already_used =>  { :info,    "You already guessed that." },
-  }
-  
-  def game_state(state) do
-    @responses[state]
-    |> alert()
-  end
-
-  defp alert(nil), do: ""
-  defp alert({class, message}) do
-    """
-    <div class="alert alert-#{class}">
-      #{message}
-    </div>
-    """
-    |> raw()
   end
 
 end
